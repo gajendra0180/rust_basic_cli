f4588d53a0115082dac6bc777fc5b8b3c06ef59a
gajendra0180 <gajendrahitz@gmail.com>
2025-05-06 18:44:27 +0000

    move helper code to helper.rs

---
+++ src/helper.rs
@@ -0,0 +1,67 @@
+use std::{
+    fs::{File, read_dir, remove_file},
+    io::{Error, ErrorKind, Result},
+    path::Path,
+};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+pub struct AddOptions {
+    #[structopt(long)]
+    item: String,
+}
+
+#[derive(StructOpt)]
+pub struct ListOptions {
+    #[structopt(long)]
+    sorted: bool,
+}
+#[derive(StructOpt)]
+pub struct RemoveOptions {
+    #[structopt(long)]
+    item: String,
+}
+
+pub fn add(folder: &str, args: &AddOptions) -> Result<()> {
+    let path = build_path_from_item(folder, &args.item);
+    if Path::new(&path).exists() {
+        Err(Error::new(
+            ErrorKind::AlreadyExists,
+            "An item with the same name already exists",
+        ))
+    } else {
+        println!("{}", path);
+        File::create(path)?;
+        Ok(())
+    }
+}
+
+pub fn list(folder: &str, args: &ListOptions) -> Result<()> {
+    let mut paths: Vec<_> = read_dir(folder)?.filter_map(Result::ok).collect();
+    if args.sorted {
+        paths.sort_by_key(|dir| dir.file_name());
+    }
+    for path in paths {
+        if let Ok(filename) = path.path().strip_prefix(folder) {
+            println!("{}", filename.display());
+        }
+    }
+    Ok(())
+}
+
+pub fn remove(folder: &str, args: &RemoveOptions) -> Result<()> {
+    let path = build_path_from_item(folder, &args.item);
+    if !Path::new(&path).exists() {
+        Err(Error::new(
+            ErrorKind::NotFound,
+            "An item with the name does not exists",
+        ))
+    } else {
+        remove_file(path)?;
+        Ok(())
+    }
+}
+
+fn build_path_from_item(folder: &str, item: &str) -> String {
+    folder.to_string() + item
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use std::{
-    fs::{File, read_dir, remove_file},
-    io::{Error, ErrorKind, Result},
-    path::Path,
-};
+mod helper;
+use std::io::Result;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -14,88 +11,24 @@ struct TodoCli {
 #[derive(StructOpt)]
 enum TodoCommand {
     #[structopt(about = "add an item", name = "add")]
-    Add(AddOptions),
+    Add(helper::AddOptions),
 
     #[structopt(about = "list all items", name = "list")]
-    List(ListOptions),
+    List(helper::ListOptions),
 
     #[structopt(about = "remove an item", name = "remove")]
-    Remove(RemoveOptions),
-}
-
-#[derive(StructOpt)]
-struct AddOptions {
-    #[structopt(long)]
-    item: String,
-}
-
-#[derive(StructOpt)]
-struct ListOptions {
-    #[structopt(long)]
-    sorted: bool,
-}
-#[derive(StructOpt)]
-struct RemoveOptions {
-    #[structopt(long)]
-    item: String,
-}
-
-fn add(folder: &str, args: &AddOptions) -> Result<()> {
-    let path = build_path_from_item(folder, &args.item);
-    if Path::new(&path).exists() {
-        Err(Error::new(
-            ErrorKind::AlreadyExists,
-            "An item with the same name already exists",
-        ))
-    } else {
-        println!("{}", path);
-        File::create(path)?;
-        Ok(())
-    }
-}
-
-fn list(folder: &str, args: &ListOptions) -> Result<()> {
-    let mut paths: Vec<_> = read_dir(folder)?.filter_map(Result::ok).collect();
-    if args.sorted {
-        paths.sort_by_key(|dir| dir.file_name());
-    }
-    for path in paths {
-        if let Ok(filename) = path.path().strip_prefix(folder) {
-            println!("{}", filename.display());
-        }
-    }
-    Ok(())
-}
-
-fn remove(folder: &str, args: &RemoveOptions) -> Result<()> {
-    let path = build_path_from_item(folder, &args.item);
-    if !Path::new(&path).exists() {
-        Err(Error::new(
-            ErrorKind::NotFound,
-            "An item with the name does not exists",
-        ))
-    } else {
-        remove_file(path)?;
-        Ok(())
-    }
-}
-
-fn build_path_from_item(folder: &str, item: &str) -> String {
-    folder.to_string() + item
+    Remove(helper::RemoveOptions),
 }
 
 fn main() -> Result<()> {
     let args = TodoCli::from_args();
     let folder = "./items/";
     match args.cmd {
-        TodoCommand::Add(args) => add(folder, &args),
-        TodoCommand::List(args) => list(folder, &args),
-        TodoCommand::Remove(args) => remove(folder, &args),
+        TodoCommand::Add(args) => helper::add(folder, &args),
+        TodoCommand::List(args) => helper::list(folder, &args),
+        TodoCommand::Remove(args) => helper::remove(folder, &args),
     }
 }
 
-
-
 // Follow ups
-// 1. move other funtions to seperate module
 // 2. add suubcommand inside subcommand and then options
